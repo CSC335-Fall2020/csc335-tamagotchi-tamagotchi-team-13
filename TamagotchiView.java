aa18c482f509de4047cf7e73f232f5d2174770f0
atushbhalla <atushbhalla@localhost>
2020-12-01 21:22:48 -0700

    Added buttons and some bar

--- TamagotchiView.java
+++ TamagotchiView.java
@@ -2,7 +2,10 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -21,19 +24,30 @@ public class TamagotchiView extends Application implements Observer{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		HBox allMechanics = new HBox();
+		Button feedSnacks = new Button("Feed Snacks");
+		Button feedMeal = new Button("Feed Meal");
+		Button feedMedicine = new Button("Give Medicine");
+		ProgressBar health = new ProgressBar();
+		feedSnacks.setDisable(true);
+		feedMeal.setDisable(true);
+		feedMedicine.setDisable(true);
+		
+		allMechanics.getChildren().addAll(feedSnacks,feedMeal,feedMedicine);
+		allMechanics.setPadding(new Insets(0,5,30,325));
+		allMechanics.setSpacing(10);
+		
 		BorderPane window = new BorderPane();
+		window.setTop(health);
+		window.setBottom(allMechanics);
 		model.addObserver(this);
 		Scene mainScene = new Scene(window, 900, 400);
 		primaryStage.setScene(mainScene);
 		primaryStage.setTitle("Tamgaochi");
 		primaryStage.show();
 	}
-	
-	/*
+		/*
 	 * GUI retieve name from user (onboarding)
 	 */
-	
-	
-	
-	
 }
