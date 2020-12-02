01e01bb831a62d6bfd31406ad1b4f9e52d5dd65d
atushbhalla <atushbhalla@localhost>
2020-12-02 13:12:53 -0700

    Added main Menu and event handlers

--- TamagotchiView.java
+++ TamagotchiView.java
@@ -1,10 +1,16 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 //import javafx.scene.canvas.Canvas;
 //import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -18,14 +24,19 @@ import javafx.scene.layout.HBox;
 //import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TamagotchiView extends Application implements Observer{
 
+	private static final int WIDTH = 900;
+	private static final int HEIGHT = 400;
+	
 	
 	TamagotchiModel model = new TamagotchiModel();
 	TamagotchiController controller = new TamagotchiController(model);
+	BorderPane root = new BorderPane();
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
@@ -36,13 +47,86 @@ public class TamagotchiView extends Application implements Observer{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		model.addObserver(this);
+		primaryStage.setScene(mainMenu(primaryStage));
+		primaryStage.setTitle("Tamgaochi");
+		primaryStage.show();
+	}
+	
+	private Scene mainMenu(Stage primaryStage) {
+		addBackground(root);
+		addMenu(primaryStage);
+		return new Scene(root, WIDTH, HEIGHT);
+	}
+	
+	private VBox menuBox = new VBox(-5);
+	
+	private void addMenu(Stage primaryStage) {
+		menuBox.setSpacing(10);
+		menuBox.setPadding(new Insets(0,0,0,20));
+		menuBox.setAlignment(Pos.CENTER_LEFT);
+		Button newGame = new Button("New Game");
+		newGame.setPrefWidth(150);
+		newGame.setFont(Font.font(15));
+		
+		newGame.setOnMouseClicked(e -> {
+			try {
+				Scene newScene = runGame();
+				primaryStage.setScene(newScene);
+				primaryStage.show();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
+		
+		Button loadGame = new Button("Load Game");
+		loadGame.setPrefWidth(150);
+		loadGame.setFont(Font.font(15));
+		
+		loadGame.setOnMouseClicked(e ->{
+			
+		});
+		
+		Button quit = new Button("Quit");
+		quit.setPrefWidth(150);
+		quit.setFont(Font.font(15));
+		
+		quit.setOnMouseClicked(e ->{
+			primaryStage.close();
+		});
+		
+		
+		menuBox.getChildren().addAll(newGame,loadGame,quit);
+		root.setLeft(menuBox);
+	}
+	
+	
+	
+	
+	private void addBackground(BorderPane root) {
+		root.setBackground(new Background(new BackgroundFill(Color.WHITE,CornerRadii.EMPTY,Insets.EMPTY)));
+	}
+	
+	private Scene runGame() throws Exception {
+		//Scene newScene = new Scene(WIDTH,HEIGHT);
+		BorderPane newRoot = new BorderPane();
+		addBackground(newRoot);
 		HBox allMechanics = new HBox();
 		Button feedSnacks = new Button("Feed Snacks");
 		Button feedMeal = new Button("Feed Meal");
 		Button feedMedicine = new Button("Give Medicine");
 		//ProgressBar health = new ProgressBar();
 		Button pause = new Button("Pause");
-		Image pet = new Image(new FileInputStream("./Pet Images/petArt.jpg"));
+		Image pet;
+		pet = null;
+		try {
+			pet = new Image(new FileInputStream("./Pet Images/petArt.jpg"));
+		}
+		catch (Exception e){
+			Alert a = new Alert(AlertType.ERROR, "Pet Image not Found");
+			a.showAndWait();
+		}
 		ImageView mainView = new ImageView(pet);
 		feedSnacks.setDisable(true);
 		feedMeal.setDisable(true);
@@ -52,18 +136,25 @@ public class TamagotchiView extends Application implements Observer{
 		allMechanics.setPadding(new Insets(0,5,30,325));
 		allMechanics.setSpacing(10);
 		
-		BorderPane window = new BorderPane();
-		window.setTop(pause);
-		window.setBackground(new Background(new BackgroundFill(Color.WHITE,CornerRadii.EMPTY,Insets.EMPTY)));
-		window.setCenter(mainView);
-		window.setBottom(allMechanics);
-		model.addObserver(this);
-		Scene mainScene = new Scene(window, 900, 400);
-		primaryStage.setScene(mainScene);
-		primaryStage.setTitle("Tamgaochi");
-		primaryStage.show();
+		newRoot.setTop(pause);
+		newRoot.setCenter(mainView);
+		newRoot.setBottom(allMechanics);
+		Scene newScene = new Scene(newRoot, WIDTH,HEIGHT);
+		return newScene;
 	}
-		/*
+	
+	private class GameFX{
+		private final BorderPane rootPane;
+		public GameFX() {
+			rootPane = new BorderPane();
+		}
+		
+		public BorderPane getRootPane() {
+	        return rootPane ;
+	    }
+	}
+	
+	/*
 	 * GUI retieve name from user (onboarding)
 	 */
 }
