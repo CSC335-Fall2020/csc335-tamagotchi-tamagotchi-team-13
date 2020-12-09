6b24dc664a3d712d02f442d565bcceb6f32557af
VTruongA <51775542+VTruongA@users.noreply.github.com>
2020-12-08 23:06:37 -0700

    Update TamagotchiController.java

--- TamagotchiController.java
+++ TamagotchiController.java
@@ -9,6 +9,10 @@ public class TamagotchiController extends Thread {
 	private final Object pauseLock = new Object();
 	private boolean paused = false;
 	
+	/**
+	 * the default constructor used for starting up a new game
+	 * @param model
+	 */
 	public TamagotchiController(TamagotchiModel model) {
 		this.model = model;
 	}
@@ -30,31 +34,54 @@ public class TamagotchiController extends Thread {
 		model.decreaseHealth(healthToDecrease);
 	}
 	
+	/**
+	 * feeds the pet by calling the model function to update the stats
+	 */
 	public void feedPetSnacks() {
 		model.feedPetSnacks();
 	}
 	
+	/**
+	 * feeds the pet by calling the model function to update the stats
+	 */
 	public void feedPetMeal() {
 		model.feedPetMeal();
 	}
 	
+	/**
+	 * gives the pet medicine by calling the model function to update the stats
+	 */
 	public void giveMedicine() {
 		model.giveMedicine();
 		model.setSick(false);
 	}
 	
+	/**
+	 * updates the paused variable by making it true
+	 */
 	public void pause() {
 		paused = true;
 	}
 	
+	/**
+	 * updates the paused variable to be false
+	 */
 	public void unPause() {
 		paused = false;
 	}
 
+	/**
+	 * creates a string to be put on the main menu
+	 * @return String the age of the pet
+	 */
 	public String getAgeDescription() {
 		return "Age: " + model.getAge();
 	}
 	
+	/**
+	 * gets the health status of the pet
+	 * @return String, the health status
+	 */
 	public String getHealthDescription() {
 		int health = model.getHealth();
 		String descr = "";
@@ -70,6 +97,10 @@ public class TamagotchiController extends Thread {
 		return descr;
 	}
 	
+	/**
+	 * gets the weight status of the pet
+	 * @return String, the weight status
+	 */
 	public String getWeightDescription() {
 		
 		int weight = model.getWeight();
@@ -88,6 +119,10 @@ public class TamagotchiController extends Thread {
 		return descr;
 	}
 	
+	/**
+	 * gets the happiness status of the pet
+	 * @return String, the happiness status
+	 */
 	public String getHappinessDescription() {
 		
 		int happiness = model.getHappiness();
@@ -102,26 +137,49 @@ public class TamagotchiController extends Thread {
 		return descr;
 	}
 
+	/**
+	 * gets the weight of the pet
+	 * @return the weight as an int
+	 */
 	public int getWeight() {
 		return model.getWeight();
 	}
 
+	/**
+	 * gets the health of the pet
+	 * @return the health as an int
+	 */
 	public int getHealth() {
 		return model.getHealth();
 	}
 
+	/**
+	 * gets the happiness of the pet
+	 * @return the happiness as an int
+	 */
 	public int getHappiness() {
 		return model.getHappiness();
 	}
 
+	/**
+	 * gets the age of the pet
+	 * @return the age as an int
+	 */
 	public int getAge() {
 		return model.getAge();
 	}
 
+	/**
+	 * updates the alive variable in the model that makes sure its dead
+	 */
 	public void petDies() {
 		model.petDies();
 	}
 	
+	/**
+	 * Begins the threads so that the game can actually run, this includes
+	 * the updating the view and decrementing the values in the model
+	 */
 	public void run() {
 		int i = 0;
 		while(!model.isDead()) {
@@ -176,6 +234,9 @@ public class TamagotchiController extends Thread {
 		
 	}
 	
+	/**
+	 * unlocks the threads so it can start up all again
+	 */
 	public void resumeGame() {
         synchronized (pauseLock) {
             paused = false;
@@ -187,7 +248,7 @@ public class TamagotchiController extends Thread {
 	/**
      * This function returns whether or not the character should get sick, 
      * based on the character's health, happiness, and weight.
-     * @return
+     * @return boolean of its sickness status
      */
     public boolean shouldGetSick() {
 
@@ -216,25 +277,51 @@ public class TamagotchiController extends Thread {
         return ThreadLocalRandom.current().nextInt(0, chancePool) == 1;
     }
 
+    /**
+     * sets the pet image locations within the model
+     * @param pet image location
+     * @param sadPet image location
+     */
 	public void setPet(String pet, String sadPet) {
 		model.setPet(pet, sadPet);
 	}
 
+	/**
+	 * gets the sad pet image location
+	 * @return String image location
+	 */
 	public String getSadPet() {
 		return model.getSadPet();
 	}
 
+	/**
+	 * gets the regular pet image location
+	 * @return String image location
+	 */
 	public String getPet() {
 		return model.getPet();
 	}
 
+	/**
+	 * calls the model function so it can load all the stats into a file
+	 * @throws FileNotFoundException
+	 */
 	public void load() throws FileNotFoundException {
 		model.load();
 	}
 
+	/**
+	 * calls the model function so it can save all the stats into a file
+	 * @throws IOException
+	 */
 	public void save() throws IOException {
 		model.save();
 	}
+	
+	/**
+	 * gets the status of if the pet is dead
+	 * @return boolean if the pet is dead
+	 */
 	public boolean isDead() {
 		return model.isDead();
 	}
